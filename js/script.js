7dff5c0d012e6f02ea69f588b908278c7ceb3d08
KaitlynIp64 <99354960+KaitlynIp64@users.noreply.github.com>
2022-03-25 19:59:29 +0000

    added code

--- js/script.js
+++ js/script.js
@@ -6,16 +6,18 @@
 
 'use strict'
 /**
- * This function calculates area of triangle.
+ * This function calculates the area of a triangle.
  */
 function calculate () {
   // input
-  const base = parseInt(document.getElementById('base-of-triangle').value)
-  const height = parseInt(document.getElementById('height-of-triangle').value)
-
+  const hoursWorked = parseInt(document.getElementById("number--of-hours-worked").value)
+  const hourlyRate = parseInt(document.getElementById("hourly-rate").value)
   // process
-  const area = (base * height) /2
-
+  const pay = (hoursWorked * hourlyRate) * (1.00 - 0.18)
+  const taxes = (hoursWorked * hourlyRate) * 0.18
   // output
-  document.getElementById('area').innerHTML = 'Area is: ' + area + ' cm²'
+  document.getElementById("payment").innerHTML =
+    "Your pay will be: " + ' $' + pay.toFixed(2)
+  document.getElementById("income-tax").innerHTML =
+    "The government will take: " + ' $' + taxes.toFixed(2)
 }
