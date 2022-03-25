6cba87f21cce405999adc6796413a9c7a269f03c
KaitlynIp64 <99354960+KaitlynIp64@users.noreply.github.com>
2022-03-25 17:28:04 +0000

    added code

---
+++ js/script.js
@@ -0,0 +1,21 @@
+// Copyright (c) 2022 Kaitlyn Ip All rights reserved
+//
+// Created by: Kaitlyn Ip
+// Created on: Mar 2022
+// This file contains the JS functions for index.html
+
+'use strict'
+/**
+ * This function calculates area of triangle.
+ */
+function calculate () {
+  // input
+  const base = parseInt(document.getElementById('base-of-triangle').value)
+  const height = parseInt(document.getElementById('height-of-triangle').value)
+
+  // process
+  const area = (base * height) /2
+
+  // output
+  document.getElementById('area').innerHTML = 'Area is: ' + area + ' cm²'
+}
